163849e07008320ec3690a3ab5e8e6f384f05b11
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-06-22 23:34:50 +0200

    add placeholder for input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -31,6 +31,7 @@ class Form extends Component {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        placeholder="Name"
         onChange={this.handleChange}
         value={this.state.name}
         />
@@ -41,6 +42,7 @@ class Form extends Component {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
+        placeholder="phone number xxx-xx-xx"
         onChange={this.handleChange}
         value={this.state.number}
         />
